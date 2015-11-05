aa42498c85ddc949fb45ace6dc76523fb391fa9b
Jason Chan <jasonfchan@gmail.com>
2015-11-04 19:51:44 -0800

    completed assignment 19

---
+++ Alocator/Alocator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Alocator
+//
+//  Created by Jason Chan MBP on 11/4/15.
+//  Copyright (c) 2015 Jason Chan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Alocator/Alocator/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Alocator
+//
+//  Created by Jason Chan MBP on 11/4/15.
+//  Copyright (c) 2015 Jason Chan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
