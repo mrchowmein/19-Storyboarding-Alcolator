46d8329cfb3fcf328179bf2c424b1d789e90af97
Jason Chan <jasonfchan@gmail.com>
2015-11-09 20:32:38 -0800

    complete checkpoint 21

---
+++ Alocator/MainMenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainMenuViewController.h
+//  
+//
+//  Created by Jason Chan MBP on 11/9/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainMenuViewController : UIViewController
+
+@end
